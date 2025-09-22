e5b311645385dd77d1503df58ed2471dcdb0d59a
Boustaaja <115188480+Boustaaja@users.noreply.github.com>
2025-09-22 21:11:55 +0300

    Add media-styles for different size screens. Fix missing check-icon for light mode

--- my-portfolio/src/sections/Hero/Hero.jsx
+++ my-portfolio/src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from "./HeroStyles.module.css";
-import heroImg from "../../assets/ProfileNoPG.png";
+import heroImg from "../../assets/ProfilePictureSized.png";
 import sun from "../../assets/sun.svg";
 import moon from "../../assets/moon.svg";
 import githubLight from "../../assets/github-light.svg";
--- my-portfolio/src/sections/Projects/Projects.jsx
+++ my-portfolio/src/sections/Projects/Projects.jsx
@@ -7,9 +7,21 @@ import ProjectCard from "../../common/ProjectCard";
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
-      <h1 className="SectionTitle">Projects</h1>
+      <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
         {/* Using the ProjectCard component to display individual projects. So far just an examples */}
+        <ProjectCard
+          src={viberr}
+          link="Linkki projektiin"
+          h3="Viberr"
+          p="Description of the project."
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="Linkki projektiin"
+          h3="Fresh Burger"
+          p="Description of the project."
+        />
         <ProjectCard
           src={viberr}
           link="Linkki projektiin"
