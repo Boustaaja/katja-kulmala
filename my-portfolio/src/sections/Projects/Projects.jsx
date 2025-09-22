0ff0c4a2691c7195fe22ae0d4f8070338b556674
Boustaaja <115188480+Boustaaja@users.noreply.github.com>
2025-09-22 15:03:40 +0300

    Add ProjectCard komponent. Add content to projectards and do linking between elements

--- my-portfolio/src/sections/Projects/Projects.jsx
+++ my-portfolio/src/sections/Projects/Projects.jsx
@@ -1,17 +1,27 @@
 import React from "react";
 import styles from "./ProjectsStyles.module.css";
 import viberr from "../../assets/viberr.png";
+import freshBurger from "../../assets/fresh-burger.png";
+import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
   return (
     <section id="projects" className={styles.container}>
       <h1 className="SectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        <a href="linkki projektiin" target="_blank">
-          <img className="hover" src={viberr} alt="Viberr logo" />
-          <h3>Viberr</h3>
-          <p>Description of the project.</p>
-        </a>
+        {/* Using the ProjectCard component to display individual projects. So far just an examples */}
+        <ProjectCard
+          src={viberr}
+          link="Linkki projektiin"
+          h3="Viberr"
+          p="Description of the project."
+        />
+        <ProjectCard
+          src={freshBurger}
+          link="Linkki projektiin"
+          h3="Fresh Burger"
+          p="Description of the project."
+        />
       </div>
     </section>
   );
