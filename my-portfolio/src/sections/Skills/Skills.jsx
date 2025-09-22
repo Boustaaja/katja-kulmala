4cee54d40965ad6d140a4eecc8b6e6a0f643b420
Boustaaja <115188480+Boustaaja@users.noreply.github.com>
2025-09-22 16:27:18 +0300

    Add new skillList and Style-sheet. Add base HTML for skillList and skills. Also bind the props. Add styles

--- my-portfolio/src/sections/Skills/Skills.jsx
+++ my-portfolio/src/sections/Skills/Skills.jsx
@@ -1,13 +1,32 @@
 import styles from "./SkillsStyles.module.css";
 import checkMarkIcon from "../../assets/checkmark-dark.svg";
+import SkillList from "../../common/SkillList";
 
 function Skills() {
   return (
-    <section id="skills" className={styles.skillsSection}>
-      <h1>Skills</h1>
-      <div className={styles.skillsContainer}>
-        <div className={styles.skillList}></div>
-        <img src={checkMarkIcon} alt="Checkmark icon" />
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="C#" />
+        <SkillList src={checkMarkIcon} skill="C++" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="HTML" />
+      </div>
+      <hr />
+      <h1 className="sectionTitle">Something</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="C++" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="HTML" />
+      </div>
+      <hr />
+      <h1 className="sectionTitle">Tools</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="C++" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="HTML" />
       </div>
     </section>
   );
